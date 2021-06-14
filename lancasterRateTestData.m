c53df32a5c87123cc084f1a28fd9c3e92617a26a
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2021-06-14 10:49:24 +0100

    Started Lancaster data importers

--- lancasterRateTestData.m
+++ lancasterRateTestData.m
@@ -1,3 +1,38 @@
 classdef lancasterRateTestData < rateTestDataImporter
-    % Abstract rate test data interface for facility correlation analysis
+    % Concrete rate test data interface for facility correlation analysis
+    % for Lancaster BATLAB data
+    
+    properties ( Constant = true )
+        Fileformat (1,1)      string                = "csv"                 % Supported input formats
+    end % abstract & constant properties
+        
+    methods
+        function obj = lancasterRateTestData( RootDir, NumHeaderLines )
+            %--------------------------------------------------------------
+            % lancasterRateTestData constructor. Imports correlation rate
+            % test data and converts it to standard format
+            %
+            % obj = lancasterRateTestData( RootDir, NumHeaderLines );
+            %
+            % Input Arguments:
+            %
+            % RootDir           --> Root directory where data is held. User 
+            %                       is prompted for the directory if empty
+            % NumHeaderLines    --> Number of header lines in file
+            %--------------------------------------------------------------
+            if ( nargin < 1 )
+                RootDir = uigetdir( cd, "Select root directory containing Lancaster rate test data" );
+            elseif ~isfolder( RootDir )
+                error( '"%s" is not a valid folder', RootDir );
+            end
+            RootDir = string( RootDir );
+            %--------------------------------------------------------------
+            % Create data store
+            %--------------------------------------------------------------
+            obj.Ds = tabularTextDatastore( RootDir, 'FileExtensions', ...
+                        obj.Fileformat, 'IncludeSubfolders', true, ...
+                        'OutputType', "table", 'TextType', "string" );
+            obj.NumHeaderLines
+        end
+    end % constructor and ordinary methods
 end % lancasterRateTestData
--- rateTestDataImporter.m
+++ rateTestDataImporter.m
@@ -1,9 +1,43 @@
 classdef ( Abstract = true ) rateTestDataImporter
-    % 
-    properties 
-    end
+    % Abstract rate test data interface class
+    
+    properties ( SetAccess = protected )
+        Ds                                                                  % File data store
+    end % protected properties
+    
+    properties ( Constant = true, Abstract = true )
+        Fileformat (1,1)      string                                        % Supported input formats
+        Tester     (1,1)      string                                        % Type of battery tester
+    end % abstract & constant properties
+    
+    methods ( Abstract = true )
+        
+    end % Abstract methods signatures
     
     methods
+        function [ T, Info ] = readDs( obj )
+            %--------------------------------------------------------------
+            % Read the next file from the datastore
+            %
+            % [ T, Info ] = obj.readDs();
+            %
+            % Output Arguments:
+            %
+            % T     --> (table) Extracted data.
+            % Info  --> Information regarding the extracted data, including
+            %           metadata.
+            %--------------------------------------------------------------
+            [ T, Info ] = read( obj.Ds );
+        end % Read the next file from the datastore
+        
+        function obj = resetDS( obj )
+            %--------------------------------------------------------------
+            % Reset the datastore to the unread state
+            %
+            % obj = obj.resetDS();
+            %--------------------------------------------------------------
+            reset( obj.Ds );
+        end % resetDS
     end % ordinary methods
     
     methods
