e2704f0174da082edbf5f3e058430febb3ebcbfa
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2021-06-14 17:38:44 +0100

    working on Lancs file importer

--- lancasterRateTestData.m
+++ lancasterRateTestData.m
@@ -3,24 +3,29 @@ classdef lancasterRateTestData < rateTestDataImporter
     % for Lancaster BATLAB data
     
     properties ( Constant = true )
-        Fileformat (1,1)      string                = "csv"                 % Supported input formats
+        Fileformat            string                = ".csv"                % Supported input formats
+        Tester                string                = "Novonix"             % Type of battery tester
     end % abstract & constant properties
-        
+       
+    properties ( SetAccess = protected )
+        Current               string                = "Current (A)"         % Name of current channel
+        Capacity              string                = "Capacity (Ah)"       % Name of capacity channel
+    end % 
+    
     methods
-        function obj = lancasterRateTestData( RootDir, NumHeaderLines )
+        function obj = lancasterRateTestData( RootDir )
             %--------------------------------------------------------------
             % lancasterRateTestData constructor. Imports correlation rate
             % test data and converts it to standard format
             %
-            % obj = lancasterRateTestData( RootDir, NumHeaderLines );
+            % obj = lancasterRateTestData( RootDir );
             %
             % Input Arguments:
             %
             % RootDir           --> Root directory where data is held. User 
             %                       is prompted for the directory if empty
-            % NumHeaderLines    --> Number of header lines in file
             %--------------------------------------------------------------
-            if ( nargin < 1 )
+            if ( nargin < 1 ) || isempty( RootDir )
                 RootDir = uigetdir( cd, "Select root directory containing Lancaster rate test data" );
             elseif ~isfolder( RootDir )
                 error( '"%s" is not a valid folder', RootDir );
@@ -29,10 +34,153 @@ classdef lancasterRateTestData < rateTestDataImporter
             %--------------------------------------------------------------
             % Create data store
             %--------------------------------------------------------------
+            warning off;
             obj.Ds = tabularTextDatastore( RootDir, 'FileExtensions', ...
                         obj.Fileformat, 'IncludeSubfolders', true, ...
                         'OutputType', "table", 'TextType', "string" );
-            obj.NumHeaderLines
+            [ obj.Ds.NumHeaderLines,  obj.Signals ] = obj.numHeaderLines();
+            warning on;
+        end
+        
+        function obj = setCurrentChannel( obj, Current )
+            %--------------------------------------------------------------
+            % Define current channel name
+            %
+            % obj = obj.setCurrentChannel( Current );
+            %
+            % Input Arguments:
+            %
+            % Current   --> (string) Name of current channel
+            %--------------------------------------------------------------
+            arguments
+                obj         lancasterRateTestData
+                Current     string                  { mustBeNonEmpty( Current ) }
+            end
+            Current = replace( Current, "(", "_" );
+            Current = replace( Current, ")", "_" );
+            Ok = obj.channelPresent( Current );
+            if Ok
+                obj.Current = Current;
+            end
+        end % setCurrentChannel
+        
+        function obj = setCapacityChannel( obj, Capacity )
+            %--------------------------------------------------------------
+            % Define capacity channel name
+            % obj = obj.setCurrentChannel( Current );
+            %
+            % Input Arguments:
+            %
+            % Capacity   --> (string) Name of capacity channel
+            %--------------------------------------------------------------
+            arguments
+                obj         lancasterRateTestData
+                Capacity    string                  { mustBeNonEmpty( Capacity ) }
+            end
+            Ok = obj.channelPresent( Capacity );
+            if Ok
+                obj.Capacity = Capacity;
+            end
         end
     end % constructor and ordinary methods
+    
+    methods 
+    end % get/set methods
+    
+    methods ( Access = protected )  
+        function SerialNumber = getSerialNumber( obj, Q )
+            %--------------------------------------------------------------
+            % Parse the battery serial number information
+            %
+            % SerialNumber = obj.getSerialNumber( Q );
+            %
+            % Input Arguments:
+            %
+            % Q   --> pointer to file
+            %--------------------------------------------------------------
+            Name = obj.Ds.Files{ Q };
+            [ ~, Name ] = fileparts( Name );
+            SerialNumber = string( extractBetween( Name, "Cell", "_" ) );
+        end
+    end % protected methods
+    
+    methods ( Access = private )
+        function [ N, Channels ] = numHeaderLines( obj )
+            %--------------------------------------------------------------
+            % Find number of header lines in the file and the list of data
+            % channels.
+            %
+            % [ N, Channels ] = obj.numHeaderLines();
+            %
+            % Output Arguments:
+            %
+            % N
+            % Channels  --> List of available channels (string)
+            %--------------------------------------------------------------
+            Fname = obj.Ds.Files{ 1 };
+            [ T, ~, N ] =importdata( Fname );
+            Channels = string( T.textdata( N, : ) );
+            N = N - 1;
+        end % numHeaderLines
+    end % private methods
+    
+    methods ( Static = true )
+        function D = calcDuration( DateTime )      
+            %--------------------------------------------------------------
+            % Calculate the test durations
+            %
+            % D = obj.calcDuration( DateTime );
+            %
+            % Input Arguments:
+            %
+            % DateTime  --> (datetime) time stamp vector for test data
+            %--------------------------------------------------------------
+            arguments 
+                DateTime  (:,1)  datetime
+            end
+            D = min( DateTime );
+            D = duration( DateTime - D );
+        end % calcDuration
+        
+       function N = numCycles( T, EventChannel )
+            %--------------------------------------------------------------
+            % Return number of cycles
+            %
+            % N = obj.numCycles( T, EventChannel );
+            %
+            % Input Arguments:
+            %
+            % T             --> (table) data table
+            % EventChannel  --> (string) Name of channel defining event
+            %
+            % Output Arguments:
+            %
+            % N             --> Number of cycles
+            %--------------------------------------------------------------
+            S = sign( T.( EventChannel ) );
+            S( S > 0 ) = 0;
+            S = diff( S );
+            N = sum( S < 0 );
+        end % numCycles  
+        
+        function [ Start, Finish ] = locEvents( T, EventChannel )
+            %--------------------------------------------------------------
+            % Locate start and finish of discharge events
+            %
+            % [ Start, Finish ] = obj.locEvents( T, , EventChannel );
+            %
+            % Input Arguments:
+            %
+            % T             --> (table) data table
+            % EventChannel  --> (string) Name of channel defining event
+            %--------------------------------------------------------------
+            S = sign( T.( EventChannel ) );
+            S( S > 0 ) = 0;
+            S = diff( S );
+            Start = find( S < 0, numel( S ), 'first' );
+            Start = Start + 1;
+            Finish = find( S > 0, numel( S ), 'first' );
+            Finish = Finish + 1;
+        end % locEvents
+    end % static methods
 end % lancasterRateTestData
--- rateTestDataImporter.m
+++ rateTestDataImporter.m
@@ -3,18 +3,64 @@ classdef ( Abstract = true ) rateTestDataImporter
     
     properties ( SetAccess = protected )
         Ds                                                                  % File data store
+        Signals     (1,:)     string                                        % List of available channels
+        Data                  table                                         % Data table
     end % protected properties
     
+    properties ( Abstract = true, SetAccess = protected )
+        Current               string                                        % Name of current channel
+        Capacity              string                                        % Name of capacity channel
+    end % Abstract & protected properties
+    
     properties ( Constant = true, Abstract = true )
-        Fileformat (1,1)      string                                        % Supported input formats
-        Tester     (1,1)      string                                        % Type of battery tester
+        Fileformat            string                                        % Supported input formats
+        Tester                string                                        % Type of battery tester
     end % abstract & constant properties
     
+    properties ( SetAccess = protected, Dependent = true )
+        NumFiles                                                            % Number of files in datastore
+    end % Dependent properties
+    
+    properties ( Access = private, Dependent = true )
+        Current_              string
+        Capacity_             string
+    end
+    
     methods ( Abstract = true )
-        
+        obj = setCurrentChannel( obj, Current )                             % Define current channel name
+        obj = setCapacityChannel( obj, Capacity )                           % Define capacity channel name
     end % Abstract methods signatures
     
+    methods ( Access = protected, Abstract = true )
+        S = getSerialNumber( obj, Q )                                       % Fetch serial number of current file
+        T = getTemperature( obj, varargin )                                 % Fetch the temperature setting
+    end % Protected abstract methods signatures
+    
+    methods ( Static = true, Abstract = true, Hidden = true )
+        D = calcDuration( DateTime )                                        % Convert timestamps to durations            
+        N = numCycles( T )                                                  % Return number of cycles  
+        [ Start, Finish ] = locEvents( C )                                  % Return start and finish of discharge events
+    end
+    
     methods
+        function Ok = channelPresent( obj, Channel )
+            %--------------------------------------------------------------
+            % Return logical value to indicate whether a user specified
+            % channel name is present in the data
+            %
+            % Ok = obj.channelPresent( Channel );
+            %
+            % Input Arguments:
+            %
+            % Channel   --> (string) Name of channel
+            %--------------------------------------------------------------
+            arguments
+                obj
+                Channel     (1,1)   string  = string.empty
+            end
+            Ok = any( strcmpi( Channel, obj.Signals ) );
+        end % channelPresent
+        
         function [ T, Info ] = readDs( obj )
             %--------------------------------------------------------------
             % Read the next file from the datastore
@@ -30,21 +76,101 @@ classdef ( Abstract = true ) rateTestDataImporter
             [ T, Info ] = read( obj.Ds );
         end % Read the next file from the datastore
         
-        function obj = resetDS( obj )
+        function obj = resetDs( obj )
             %--------------------------------------------------------------
             % Reset the datastore to the unread state
             %
-            % obj = obj.resetDS();
+            % obj = obj.resetDs();
             %--------------------------------------------------------------
             reset( obj.Ds );
-        end % resetDS
+        end % resetDs
+        
+        function obj = extractData( obj, FileName )
+            %--------------------------------------------------------------
+            % Extract data from the datastore & write to the 
+            %
+            % obj = obj.extractData( FileName );
+            %
+            % Input Arguments:
+            %
+            % FileName  --> (string) Name of output file
+            %--------------------------------------------------------------
+            arguments
+                obj
+                FileName (1,1) string  { mustBeNonempty( FileName ) }
+            end
+            %--------------------------------------------------------------
+            % Make sure the output file is an xlsx file
+            %--------------------------------------------------------------
+            FileName = obj.makeExcelFile( FileName );
+            obj = obj.resetDs();
+            N = obj.NumFiles;
+            for Q = 1:N
+                %----------------------------------------------------------
+                % Fetch the necessary data one file at a time and append to
+                % a data table for export
+                %----------------------------------------------------------
+                SerialNumber = obj.getSerialNumber( Q );
+                T = obj.readDs();
+                NumCyc = obj.numCycles( T, obj.Current_ );
+                [ Start, Finish ] = obj.locEvents( T, obj.Current_ );
+                Cycle = ( 1:NumCyc ).';
+                DischargeCapacity = T{ Start, obj.Capacity_ } -....
+                                    T{ Finish, obj.Capacity_ };
+                SerialNumber = repmat( SerialNumber, NumCyc, 1 );
+            end
+        end % extractData
     end % ordinary methods
     
     methods
-    end % get/set methods
+        function N = get.NumFiles( obj )
+            % Return number of files in datastore
+            N = numel( obj.Ds.Files );
+        end 
+        
+        function C = get.Current_( obj )
+            % Return parsed current channel
+            C = obj.parseChannelName( obj.Current );
+        end
+        
+        function C = get.Capacity_( obj )
+            % Return parsed capacity channel
+            C = obj.parseChannelName( obj.Capacity );
+        end    end % get/set methods
     
     methods ( Access = protected )
     end % protected methods    
     
-    
+    methods ( Static = true, Access = protected )
+        function ExcelFile = makeExcelFile( FileName )
+            %--------------------------------------------------------------
+            % Make sure the export file is an ".xlsx" file
+            %
+            % ExcelFile = obj.makeExcelFile( FileName )
+            %
+            % Input Arguments:
+            %
+            % FileName  --> (string) Name of file
+            %--------------------------------------------------------------
+            [ Fpath, ExcelFile ] = fileparts( FileName );
+            ExcelFile = strjoin( [ ExcelFile, "xlsx" ], "." );
+            ExcelFile = fullfile( Fpath, ExcelFile );
+        end % makeExcelFile
+        
+        function Name = parseChannelName( Channel )
+            %--------------------------------------------------------------
+            % Remove spaces and parentheses
+            %
+            % Name = obj.parseChannelname( Channel );
+            %
+            % Input Arguments:
+            %
+            % Channel   --> (string) Name of channel to parse
+            %--------------------------------------------------------------
+            Name = replace( Channel, " ", "" );
+            Name = replace( Name, "(", "_" );
+            Name = replace( Name, ")", "_" );
+            
+        end
+    end % protected static methods
 end % rateTestDataImporter
