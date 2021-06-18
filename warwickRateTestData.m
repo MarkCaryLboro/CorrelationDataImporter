67604cd93656d1af594a8ea23c030895b08f59c5
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2021-06-18 08:58:41 +0100

    Update warwickRateTestData.m

--- warwickRateTestData.m
+++ warwickRateTestData.m
@@ -9,8 +9,8 @@ classdef warwickRateTestData < rateTestDataImporter
     end % abstract & constant properties   
     
     properties ( SetAccess = protected )
-        Current               string                = "Current (A)"         % Name of current channel
-        Capacity              string                = "Capacity (Ah)"       % Name of capacity channel
+        Current               string                = "Current"             % Name of current channel
+        Capacity              string                = "AhAccu"              % Name of capacity channel
     end % protected properties    
     
     methods
@@ -55,8 +55,23 @@ classdef warwickRateTestData < rateTestDataImporter
             %
             % obj = obj.extractData( FileName );
             %
-            % 
+            % Input Arguments:
+            %
+            % FileName  --> (string) Full file specification for index file
+            %               If empty user will be prompted for the file
+            %               name.
             %--------------------------------------------------------------
+            if ( nargin < 2 ) || ~isfile( FileName )
+                [ FileName, Fpath ] = uigetfile( ".xlsx",...
+                    "Select rate test index file", "Rate_Crate&Temp.xlsx",...
+                    'MultiSelect', 'off' );
+               FileName = fullfile( Fpath, FileName );
+            end
+            if isfile( FileName )
+                Idx = obj.makeIndexTable( FileName );
+            else
+                error( 'Must Supply a Valid File Name' );
+            end
             obj = obj.resetDs();
             obj.Data = table.empty;
             N = obj.NumFiles;
@@ -75,24 +90,25 @@ classdef warwickRateTestData < rateTestDataImporter
                 %----------------------------------------------------------
                 % Capture metadata
                 %----------------------------------------------------------
-                SerialNumber = obj.getSerialNumber( Q );
-                Temperature = obj.getTemperature( Q );
-                CRate = obj.getCrate( Q ); 
+                SerialNumber = Idx.SerialNumber( Q );
+                Temperature = Idx.Temperature( Q );
+                CRate = string( Idx.CRate{ Q } ); 
                 %----------------------------------------------------------
                 % Read the current file
                 %----------------------------------------------------------
                 T = obj.readDs();
+                T.data = obj.interpData( T.data );
                 %----------------------------------------------------------
                 % Calculate number and location of the discharge events
                 %----------------------------------------------------------
-                NumCyc = obj.numCycles( T, obj.Current_ );
-                [ Start, Finish ] = obj.locEvents( T, obj.Current_ );
+                NumCyc = obj.numCycles( T.data, obj.Current_ );
+                [ Start, Finish ] = obj.locEvents( T.data, obj.Current_ );
                 Cycle = ( 1:NumCyc ).';
                 %----------------------------------------------------------
                 % Calculate the discharge capacity
                 %----------------------------------------------------------
-                DischargeCapacity = T{ Start, obj.Capacity_ } -....
-                                    T{ Finish, obj.Capacity_ };
+                DischargeCapacity = T.data.( obj.Capacity_ )( Start ) -....
+                                    T.data.( obj.Capacity_ )( Finish );
                 %----------------------------------------------------------
                 % Write the curreent data to a summary data and append it 
                 % to the data table
@@ -100,10 +116,10 @@ classdef warwickRateTestData < rateTestDataImporter
                 SerialNumber = repmat( SerialNumber, NumCyc, 1 );
                 Temperature = repmat( Temperature, NumCyc, 1 );
                 CRate = repmat( CRate, NumCyc, 1 );
-                Facility = string( repmat( obj.Facility, NumCyc, 1 ) );     %#ok<PROP>
+                Facility = string( repmat( obj.Facility, NumCyc, 1 ) );      %#ok<PROPLC>
                 BatteryName = repmat( obj.Battery, NumCyc, 1 );
                 T = table( BatteryName, SerialNumber, CRate, Cycle,...
-                           Facility, Temperature, DischargeCapacity );      %#ok<PROP>
+                           Facility, Temperature, DischargeCapacity );       %#ok<PROPLC>
                 if isempty( obj.Data )
                     obj.Data = T;
                 else
@@ -127,44 +143,107 @@ classdef warwickRateTestData < rateTestDataImporter
     end
     
     methods ( Access = protected )
-        function S = getSerialNumber( obj, Q, Str )   
-            %--------------------------------------------------------------
-            % Fetch serial number of current file
-            % 
-            % S = obj.getSerialNumber( Q, Str );
-            %
-            % Input Arguments:
-            %
-            % Q   --> pointer to file
-            % Str --> search string.
-            %--------------------------------------------------------------
-            if ( nargin < 3 )
-                Str = "Cell";
-            end
-            [ ~, Fname ] = fileparts( obj.Ds.Files{ Q } );
-            S = string( extractBetween( Fname, Str, "_" ));
-        end % getSerialNumber
-        
-        function T = getTemperature( obj, Q, Str )                                   
-            % Fetch the temperature setting
-        end % getTemperature
-        
-        function C = getCrate( obj, Q, Str ) 
-            % Fetch the c-rate data
-        end % getCrate
     end % protected methods
     
+    methods ( Access = private )
+    end % private methods
+    
     methods ( Static = true, Hidden = true )
+        function T = makeIndexTable( Fname )
+            %--------------------------------------------------------------
+            % Generate the index table necessary for assigning the settings
+            % to the data
+            %
+            % T = obj.makeIndexTable( Fname );
+            % 
+            % Input Arguments:
+            %
+            % Fname     --> Full file specification for index file
+            %--------------------------------------------------------------
+            [ ~, ~, T ] = xlsread( Fname, 1 );
+            Vars = string( T( 1, : ) );
+            %--------------------------------------------------------------
+            % Use standard names
+            %--------------------------------------------------------------
+            Idx = strcmpi( "C_rate", Vars );
+            Vars( Idx ) = "CRate";
+            Idx = strcmpi( "CellNumber", Vars );
+            Vars( Idx ) = "SerialNumber";
+            T = T( 2:end, : );
+            T = cell2table( T );
+            T.Properties.VariableNames = Vars;
+        end % makeIndexTable
+        
         function D = calcDuration( DateTime ) 
             % Convert timestamps to durations  
         end %calcDuration
         
-        function N = numCycles( T )                                                  
-            % Return number of cycles  
-        end % numCycles
+       function N = numCycles( T, EventChannel )
+            %--------------------------------------------------------------
+            % Return number of cycles
+            %
+            % N = obj.numCycles( T, EventChannel );
+            %
+            % Input Arguments:
+            %
+            % T             --> (table) data table
+            % EventChannel  --> (string) Name of channel defining event
+            %
+            % Output Arguments:
+            %
+            % N             --> Number of cycles
+            %--------------------------------------------------------------
+            S = sign( T.( EventChannel ) );
+            S( S > 0 ) = 0;
+            S = diff( S );
+            N = sum( S < 0 );
+        end % numCycles 
         
-        function [ Start, Finish ] = locEvents( C )  
-            % Return start and finish of discharge events
+        function [ Start, Finish ] = locEvents( T, EventChannel )
+            %--------------------------------------------------------------
+            % Locate start and finish of discharge events
+            %
+            % [ Start, Finish ] = obj.locEvents( T, , EventChannel );
+            %
+            % Input Arguments:
+            %
+            % T             --> (table) data table
+            % EventChannel  --> (string) Name of channel defining event
+            %--------------------------------------------------------------
+            S = sign( T.( EventChannel ) );
+            S( S > 0 ) = 0;
+            S = diff( S );
+            Start = find( S < 0, numel( S ), 'first' );
+            Start = Start + 1;
+            Finish = find( S > 0, numel( S ), 'first' );
+            Finish = Finish + 1;
         end % locEvents
+        
+        function S = interpData( S )
+            %--------------------------------------------------------------
+            % Reinterpolate data to remove NaNs
+            %
+            % S = obj.interpData( S );
+            %
+            % Input Arguments:
+            %
+            % S     --> Data structure
+            %--------------------------------------------------------------
+            Names = string( fieldnames( S ) );
+            N = numel( Names );
+            for Q = 1:N
+                %----------------------------------------------------------
+                % If any nans present re-interpolate
+                %----------------------------------------------------------
+                if any( isnan( S.( Names ) ) )
+                    D = S.( Names );
+                    X = ( 1:numel( D ) ).';
+                    Idx = ~isnan( D );
+                    D = D( Idx );
+                    S.( Names ) = interp1( D, X( Idx ), X, 'linear',...
+                                           'extrap' );
+                end
+            end
+        end % interpData
     end % Static & hidden methods
 end % warwickRateTestData
