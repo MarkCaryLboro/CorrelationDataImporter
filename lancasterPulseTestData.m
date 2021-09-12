67f37b79267232d0f325bf38bed8d54064f43b5d
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2021-09-12 09:46:35 +0100

    Begun work on pulse test importers

---
+++ lancasterPulseTestData.m
@@ -0,0 +1,7 @@
+classdef lancasterPulseTestData < pulseTestDataImporter
+    % Concrete pulse test data interface for facility correlation analysis
+    % for Lancaster BATLAB data
+    
+    
+    
+end % lancasterPulseTestData
